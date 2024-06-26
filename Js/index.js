b60ca995462ff92365b03d5afa74b7d937b4b707
Ripanul Alam Ridoy <ripanulalam8@gmail.com>
2024-06-26 20:37:55 +0600

    small desvece responsibe

--- Js/index.js
+++ Js/index.js
@@ -8,47 +8,10 @@ window.addEventListener('scroll', function() {
   });
 
 
-/*
-
-window.addEventListener('scroll', function() {...}):
-
-    This line of JavaScript is adding an event listener to the window object. An event listener is a function that waits for an event to occur, in this case, the scroll event. This means that the function provided inside the parentheses will be executed whenever the user scrolls.
-    
-
-
-    
-var navbar = document.querySelector('.navbar');:
-
-    This line is declaring a variable named navbar and assigning it to the element with the class name 'navbar' on your webpage. document.querySelector('.navbar') returns the first element in the document that matches the specified selector, in this case, '.navbar'.
-    
-    
-
-
-if (window.pageYOffset > 0) {...}:
-
-    This is an if statement that checks if the page has been scrolled down. window.pageYOffset is a built-in property that returns the number of pixels that the document has been scrolled vertically. If the number of pixels is greater than 0, it means the user has scrolled down the page.
-    
-
-
-navbar.classList.add('scrolled');:
-
-    If the page has been scrolled down, this line adds the class 'scrolled' to the navbar element. When the 'scrolled' class is added, it changes the style of the navbar according to the CSS rules you set for the 'scrolled' class (like changing the background color).
-    
-
-else {navbar.classList.remove('scrolled');}:
-
-    If the page has not been scrolled down (i.e., if window.pageYOffset is not greater than 0), then the 'scrolled' class is removed from the navbar element. This means that when the user scrolls back up to the top of the page, the navbar will revert back to its original style as defined in the CSS rules for the 'navbar' class.
-    
-*/
-
-
-
-
-//carousel code start
 
 const myCarouselElement = document.querySelector('#carouselExampleControls');
 const carousel = new bootstrap.Carousel(myCarouselElement, {
-  interval: 4000,
+  interval: 3000,
   wrap: true
 })
 
@@ -57,14 +20,9 @@ const carousel = new bootstrap.Carousel(myCarouselElement, {
 document.addEventListener("DOMContentLoaded", function() {
   const reviewCarouselElement = document.querySelector('#carouselExampleIndicators2');
   const reviewcarousel = new bootstrap.Carousel(reviewCarouselElement, {
-    interval: 2000,
+    interval: 3000,
     wrap: true
   });
-});
-
-
-
-
 
 // form valid massage start
 const form=document.getElementById('forms')
@@ -92,7 +50,7 @@ function chequeInputs(){
     const selectValue = select.value.trim();
 
 
-    if(nameValue=== ''){
+    if(nameValue === ''){
         //add error class and show error
         setErrorFor(name, 'Name cannot not be empty');
     }else{
@@ -153,6 +111,18 @@ function setSuccess2For(input){
     const formControl = input.parentElement; // class .form-control
     formControl.className ='col-lg-4 col-sm-6 position-relative form-label success';
 
-}
+};
+
+});
+
+
+
+
+
+
   
+
+
+
+
 
